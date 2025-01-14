4647656fc16465b2ebe70342deb66787ce1da8d3
Yeonjae <lion721721@gmail.com>
2025-01-15 01:32:25 +0900

    Time: 7 ms (46.7%), Space: 25.7 MB (11.47%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Rotates the array to the right by k steps in-place.
+        """
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+
+    def reverse(self, nums: list[int], start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+        
