2c0f538677e2586f07776c3728e63471fc564a27
Yeonjae <lion721721@gmail.com>
2025-01-28 20:12:26 +0900

    Time: 7 ms (80.53%), Space: 19 MB (11.62%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,11 +5,14 @@ class Solution:
 
         pos, jump, jump_cnt = 0, nums[0], 1
         while True:
-            max_pos = 0
-            for idx in range(pos, pos+jump):
+            max_pos, new_pos = 0, 0
+            print(pos, jump)
+            for idx in range(pos, pos+jump+1):
                 if idx < len(nums) and max_pos < idx + nums[idx]:
                     max_pos = idx + nums[idx]
+                    new_pos = idx
                 if idx == len(nums)-1:
                     return jump_cnt
             jump_cnt += 1
-            pos = max_pos
+            pos = new_pos
+            jump = nums[pos]
